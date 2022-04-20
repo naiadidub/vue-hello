8ad51d29c0842671228db2ce6f0f0c364b5dab51
naiadidub <antonio.zaghi1405@gmail.com>
2022-04-20 17:26:31 +0200

    end

---
+++ main.js
@@ -0,0 +1,11 @@
+const app = new Vue (
+    {
+        el: "#app",
+        data: {
+            messaggio: 'ciao vue',
+            src: 'https://i0.wp.com/fattoalatina.it/wp-content/uploads/2020/06/santa-maria-goretti.jpg?fit=750%2C430&ssl=1',
+            nomeAletrnativo: 'Maria Antonietta'
+        }
+
+    }
+)
